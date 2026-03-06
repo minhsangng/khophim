c79930b5c90a2e83fbcaaa48a9cd0ae5139000e5
minhsangng <nguyenminhsang.work@gmail.com>
2026-03-06 08:33:51 +0700

    Update content home

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
 const baseUrl = "https://ophim1.com/v1/api/";
+const imageUrl = "https://img.ophim.live/uploads/movies/";
 
 const options = {
   method: "GET",
@@ -27,6 +28,39 @@ async function addCategoriesNav() {
       });
 
       categoryNav.appendChild(fragment);
+    }
+  }
+}
+
+async function getCategoriesList() {
+  const categoryList = document.getElementById("category-list");
+  let item = ``;
+
+  if (categoryList) {
+    const datas = await callAPI("the-loai");
+
+    if (datas.status == "success") {
+      datas.data.items.forEach((e) => {
+        item += `<div class="swiper-slide">
+                  <div class="product-item image-zoom-effect link-effect">
+                    <div class="image-holder position-relative">
+                      <a href="" class="btn-icon btn-wishlist">
+                        <svg width="24" height="24" viewBox="0 0 24 24">
+                          <use xlink:href="#heart"></use>
+                        </svg>
+                      </a>
+                      <div class="product-content">
+                        <h5 class="element-title text-uppercase fs-5 mt-3">
+                          <a href="">${e.name}</a>
+                        </h5>
+                        <a href="#" class="text-decoration-none" data-after="Xem ngay"><span>${e.slug}</span></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
+      });
+
+      categoryList.innerHTML = item;
     }
   }
 }
@@ -88,7 +122,7 @@ async function getNewMovie() {
   let item = ``;
 
   if (newMovie) {
-    const datas = await callAPI("danh-sach/phim-moi");
+    const datas = await callAPI("home");
 
     if (datas.status == "success") {
       datas.data.items.forEach((e) => {
@@ -96,7 +130,7 @@ async function getNewMovie() {
               <div class="banner-item image-zoom-effect">
                 <div class="image-holder group relative">
                   <a href="/phim/${e.slug}">
-                    <img src="https://img.ophim.live/uploads/movies/${e.thumb_url}" alt="Thumbnail" class="img-fluid w-full h-[500px] object-cover">
+                    <img src="${imageUrl}${e.thumb_url}" alt="Thumbnail" class="img-fluid w-full h-[500px] object-cover">
                   </a>
                   <a href="/phim/${e.slug}" class="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 group-hover:block hidden" title="Xem ngay">
                     <ion-icon name="play-circle-outline" class="size-24 rounded-full text-amber-200"></ion-icon>
@@ -116,18 +150,94 @@ async function getNewMovie() {
   }
 }
 
-/* function targetAPINavbar() {
-  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+async function getTopRating() {
+  const topRating = document.getElementById("top-rating");
+  let col = ``;
 
-  navLinks.forEach((link) => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
+  if (topRating) {
+    const datas = await callAPI("danh-sach/phim-moi");
 
-      const href = link.href.split(window.location.origin.toString() + "/")[1];
-      const target = baseUrl + href;
-    });
-  });
-} */
+    if (datas.status == "success") {
+      const currentYear = new Date().getFullYear();
+
+      const filtered = datas.data.items.filter(
+        (e) =>
+          e.imdb && Number(e.year) === currentYear && e.imdb.vote_average,
+      );
+      
+      const sorted = filtered.sort(
+        (a, b) => Number(b.imdb.vote_average) - Number(a.imdb.vote_average),
+      );
+
+      const top3 = sorted.slice(0, 3);
+
+      top3.forEach((e, index) => {
+        col += `
+          <div class="col-md-4">
+            <div class="cat-item image-zoom-effect relative">
+              <div class="image-holder">
+                <a href="/phim/${e.slug}">
+                <img src="${imageUrl}${e.thumb_url}" alt="movie" class="product-image img-fluid">
+                <div class="absolute top-0 left-0 w-14 h-20 rounded-bl-full rounded-br-full bg-red-400/75 flex items-center justify-center">
+                  <span class="block text-white">${e.imdb.vote_average}</span>
+                </div>
+                </a>
+              </div>
+              <div class="category-content w-full">
+                <div class="product-button text-center">
+                  <span class="btn btn-common text-uppercase font-bold text-black text-3xl">
+                    Top ${index + 1}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>`;
+      });
+
+      topRating.innerHTML = col;
+    }
+  }
+}
+
+async function getCountryList() {
+  const countryList = document.getElementById("country-list");
+  let item = ``;
+
+  if (countryList) {
+    const datas = await callAPI("quoc-gia");
+
+    if (datas.status == "success") {
+
+      const sorted = datas.data.items.sort((a, b) => {
+
+        if (a.slug === "quoc-gia-khac") return 1;
+        if (b.slug === "quoc-gia-khac") return -1;
+
+        return a.name.localeCompare(b.name, "vi");
+      });
+
+      sorted.forEach((e) => {
+        item += `<div class="swiper-slide">
+            <div class="product-item image-zoom-effect link-effect">
+              <div class="image-holder flex justify-between items-center">
+                <img src="images/co-quoc-gia/${e.slug}.webp" alt="${e.slug}" width="100px" height="50px" class="product-image img-fluid border border-green-500 shadow">
+                <div class="product-content text-right w-1/2">
+                  <h5 class="text-uppercase fs-5 mt-3">
+                    <a href="">${e.name}</a>
+                  </h5>
+                  <a href="" class="text-decoration-none" data-after="Xem ngay">
+                    <span>${e.slug}</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>`;
+      });
+
+      countryList.innerHTML = item;
+    }
+  }
+}
 
 async function callAPI(type) {
   try {
@@ -143,11 +253,12 @@ function initPage() {
   Promise.all([
     addCategoriesNav(),
     addCountriesNav(),
-    addCatLists().
+    addCatLists(),
     getNewMovie(),
+    getTopRating(),
+    getCategoriesList(),
+    getCountryList(),
   ]);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  initPage();
-});
+window.onload = initPage;
